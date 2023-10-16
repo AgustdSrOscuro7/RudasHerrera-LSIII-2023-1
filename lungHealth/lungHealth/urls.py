ace06e3ffaea86433e0ca318a641e75b100ec14b
AgustdSrOscuro7 <valekrudas1@gmail.com>
2023-10-16 14:53:04 -0500

    ajustes

--- lungHealth/lungHealth/urls.py
+++ lungHealth/lungHealth/urls.py
@@ -1,22 +1,50 @@
-"""
-URL configuration for lungHealth project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
+from lungHealth_app.views import (
+    MedicoListado, MedicoDetalle, MedicoCrear, MedicoActualizar, MedicoEliminar,
+    PacienteListado, PacienteDetalle, PacienteCrear, PacienteActualizar, PacienteEliminar,
+    DiagnosticoListado, DiagnosticoDetalle, DiagnosticoCrear, DiagnosticoActualizar, DiagnosticoEliminar,
+    TratamientoListado, TratamientoDetalle, TratamientoCrear, TratamientoActualizar, TratamientoEliminar,
+    HospitalListado, HospitalDetalle, HospitalCrear, HospitalActualizar, HospitalEliminar,
+)
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('crear_medico')), name='inicio'),  # Redireccionar página de inicio a creación de médicos
+
+    # URLs para Médicos
     path('admin/', admin.site.urls),
+    path('medicos/', MedicoListado.as_view(template_name="medicos/index.html"), name='leer_medicos'),
+    path('medicos/detalle/<int:pk>/', MedicoDetalle.as_view(template_name="medicos/detalles.html"), name='detalles_medico'),
+    path('medicos/crear/', MedicoCrear.as_view(template_name="medicos/crear.html"), name='crear_medico'),
+    path('medicos/editar/<int:pk>/', MedicoActualizar.as_view(template_name="medicos/actualizar.html"), name='actualizar_medico'),
+    path('medicos/eliminar/<int:pk>/', MedicoEliminar.as_view(template_name="medicos/eliminar.html"), name='eliminar_medico'),
+
+    # URLs para Pacientes
+    path('pacientes/', PacienteListado.as_view(template_name="paciente/index.html"), name='leer_pacientes'),
+    path('pacientes/detalle/<int:pk>/', PacienteDetalle.as_view(template_name="pacientes/detalles.html"), name='detalles_paciente'),
+    path('pacientes/crear/', PacienteCrear.as_view(template_name="pacientes/crear.html"), name='crear_paciente'),
+    path('pacientes/editar/<int:pk>/', PacienteActualizar.as_view(template_name="pacientes/actualizar.html"), name='actualizar_paciente'),
+    path('pacientes/eliminar/<int:pk>/', PacienteEliminar.as_view(template_name="pacientes/eliminar.html"), name='eliminar_paciente'),
+
+    # URLs para Diagnósticos
+    path('diagnosticos/', DiagnosticoListado.as_view(template_name="diagnosticos/index.html"), name='leer_diagnosticos'),
+    path('diagnosticos/detalle/<int:pk>/', DiagnosticoDetalle.as_view(template_name="diagnosticos/detalles.html"), name='detalles_diagnostico'),
+    path('diagnosticos/crear/', DiagnosticoCrear.as_view(template_name="diagnosticos/crear.html"), name='crear_diagnostico'),
+    path('diagnosticos/editar/<int:pk>/', DiagnosticoActualizar.as_view(template_name="diagnosticos/actualizar.html"), name='actualizar_diagnostico'),
+    path('diagnosticos/eliminar/<int:pk>/', DiagnosticoEliminar.as_view(template_name="diagnosticos/eliminar.html"), name='eliminar_diagnostico'),
+
+    # URLs para Tratamientos
+    path('tratamientos/', TratamientoListado.as_view(template_name="tratamientos/index.html"), name='leer_tratamientos'),
+    path('tratamientos/detalle/<int:pk>/', TratamientoDetalle.as_view(template_name="tratamientos/detalles.html"), name='detalles_tratamiento'),
+    path('tratamientos/crear/', TratamientoCrear.as_view(template_name="tratamientos/crear.html"), name='crear_tratamiento'),
+    path('tratamientos/editar/<int:pk>/', TratamientoActualizar.as_view(template_name="tratamientos/actualizar.html"), name='actualizar_tratamiento'),
+    path('tratamientos/eliminar/<int:pk>/', TratamientoEliminar.as_view(template_name="tratamientos/eliminar.html"), name='eliminar_tratamiento'),
+
+    # URLs para Hospitales
+    path('hospitales/', HospitalListado.as_view(template_name="hospitales/index.html"), name='leer_hospitales'),
+    path('hospitales/detalle/<int:pk>/', HospitalDetalle.as_view(template_name="hospitales/detalles.html"), name='detalles_hospital'),
+    path('hospitales/crear/', HospitalCrear.as_view(template_name="hospitales/crear.html"), name='crear_hospital'),
+    path('hospitales/editar/<int:pk>/', HospitalActualizar.as_view(template_name="hospitales/actualizar.html"), name='actualizar_hospital'),
+    path('hospitales/eliminar/<int:pk>/', HospitalEliminar.as_view(template_name="hospitales/eliminar.html"), name='eliminar_hospital'),
 ]
--- lungHealth/lungHealth_app/views.py
+++ lungHealth/lungHealth_app/views.py
@@ -1,3 +1,166 @@
 from django.shortcuts import render
+from .models import Medico, Paciente, Diagnostico, Tratamiento, PruebaMedica, NotaEnfermeria, Hospital
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'lungHealth/index.html')
+
+# Vistas para Médicos
+class MedicoListado(ListView):
+    model = Medico
+
+class MedicoCrear(SuccessMessageMixin, CreateView):
+    model = Medico
+    fields = "__all__"
+    success_message = 'Médico Creado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_medicos')
+
+class MedicoDetalle(DetailView):
+    model = Medico
+
+class MedicoActualizar(SuccessMessageMixin, UpdateView):
+    model = Medico
+    fields = "__all__"
+    success_message = 'Médico Actualizado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_medicos')
+
+class MedicoEliminar(SuccessMessageMixin, DeleteView):
+    model = Medico
+
+    def get_success_url(self):
+        success_message = 'Médico Eliminado Correctamente!'
+        messages.success(self.request, success_message)
+        return reverse('leer_medicos')
+
+# Vistas para Pacientes
+class PacienteListado(ListView):
+    model = Paciente
+
+class PacienteCrear(SuccessMessageMixin, CreateView):
+    model = Paciente
+    fields = "__all__"
+    success_message = 'Paciente Creado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_pacientes')
+
+class PacienteDetalle(DetailView):
+    model = Paciente
+
+class PacienteActualizar(SuccessMessageMixin, UpdateView):
+    model = Paciente
+    fields = "__all__"
+    success_message = 'Paciente Actualizado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_pacientes')
+
+class PacienteEliminar(SuccessMessageMixin, DeleteView):
+    model = Paciente
+
+    def get_success_url(self):
+        success_message = 'Paciente Eliminado Correctamente!'
+        messages.success(self.request, success_message)
+        return reverse('leer_pacientes')
+
+# Vistas para Diagnósticos
+class DiagnosticoListado(ListView):
+    model = Diagnostico
+
+class DiagnosticoCrear(SuccessMessageMixin, CreateView):
+    model = Diagnostico
+    fields = "__all__"
+    success_message = 'Diagnóstico Creado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_diagnosticos')
+
+class DiagnosticoDetalle(DetailView):
+    model = Diagnostico
+
+class DiagnosticoActualizar(SuccessMessageMixin, UpdateView):
+    model = Diagnostico
+    fields = "__all__"
+    success_message = 'Diagnóstico Actualizado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_diagnosticos')
+
+class DiagnosticoEliminar(SuccessMessageMixin, DeleteView):
+    model = Diagnostico
+
+    def get_success_url(self):
+        success_message = 'Diagnóstico Eliminado Correctamente!'
+        messages.success(self.request, success_message)
+        return reverse('leer_diagnosticos')
+
+# Vistas para Tratamientos
+class TratamientoListado(ListView):
+    model = Tratamiento
+
+class TratamientoCrear(SuccessMessageMixin, CreateView):
+    model = Tratamiento
+    fields = "__all__"
+    success_message = 'Tratamiento Creado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_tratamientos')
+
+class TratamientoDetalle(DetailView):
+    model = Tratamiento
+
+class TratamientoActualizar(SuccessMessageMixin, UpdateView):
+    model = Tratamiento
+    fields = "__all__"
+    success_message = 'Tratamiento Actualizado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_tratamientos')
+
+class TratamientoEliminar(SuccessMessageMixin, DeleteView):
+    model = Tratamiento
+
+    def get_success_url(self):
+        success_message = 'Tratamiento Eliminado Correctamente!'
+        messages.success(self.request, success_message)
+        return reverse('leer_tratamientos')
+
+
+# Vistas para Hospitales
+class HospitalListado(ListView):
+    model = Hospital
+
+class HospitalCrear(SuccessMessageMixin, CreateView):
+    model = Hospital
+    fields = "__all__"
+    success_message = 'Hospital Creado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_hospitales')
+
+class HospitalDetalle(DetailView):
+    model = Hospital
+
+class HospitalActualizar(SuccessMessageMixin, UpdateView):
+    model = Hospital
+    fields = "__all__"
+    success_message = 'Hospital Actualizado Correctamente!'
+
+    def get_success_url(self):
+        return reverse('leer_hospitales')
+
+class HospitalEliminar(SuccessMessageMixin, DeleteView):
+    model = Hospital
+
+    def get_success_url(self):
+        success_message = 'Hospital Eliminado Correctamente!'
+        messages.success(self.request, success_message)
+        return reverse('leer_hospitales')
